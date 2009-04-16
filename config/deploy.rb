baa4296f2ab0683555a07361e1aff92c58bff779
Michał Kurek <oki@md6.org>
2009-04-16 09:33:55 +0200

    updated capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,35 @@
-set :stages, %w(staging production)
-set :default_stage, "production"
-require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
+require 'capistrano/ext/multistage'
+#set :stages, %w(production beta)
 
+#set :default_stage, 'beta'
+#set :default_env, 'beta'
 
-namespace :db do
-  desc 'Dumps the production database to db/production_data.sql on the remote server'
-  task :remote_db_dump, :roles => :db, :only => { :primary => true } do
-    run "cd #{deploy_to}/#{current_dir} && " +
-      "rake RAILS_ENV=#{rails_env} db:database_dump --trace" 
+set :application, "app_name"
+
+set :deploy_to, "/var/www/apps/#{application}"
+
+set :scm, :git
+set :repository, "git@3.xserve.pl:#{application}.git"
+
+# set :branch, "production"
+# set :deploy_via, :remote_cache
+
+# set :user, 'oki'
+set :user, 'git'
+set :ssh_options, { :paranoid => false }
+
+set :use_sudo, false
+
+set :keep_releases, 5
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 
-  desc 'Downloads db/production_data.sql from the remote production environment to your local machine'
-  task :remote_db_download, :roles => :db, :only => { :primary => true } do  
-    execute_on_servers(options) do |servers|
-      self.sessions[servers.first].sftp.connect do |tsftp|
-        tsftp.download!("#{deploy_to}/#{current_dir}/db/production_data.sql", "db/production_data.sql")
-      end
-    end
-  end
-
-  desc 'Cleans up data dump file'
-  task :remote_db_cleanup, :roles => :db, :only => { :primary => true } do
-    execute_on_servers(options) do |servers|
-      self.sessions[servers.first].sftp.connect do |tsftp|
-        tsftp.remove! "#{deploy_to}/#{current_dir}/db/production_data.sql" 
-      end
-    end
-  end 
-
-  desc 'Dumps, downloads and then cleans up the production data dump'
-  task :remote_db_runner do
-    remote_db_dump
-    remote_db_download
-    remote_db_cleanup
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
   end
 end
